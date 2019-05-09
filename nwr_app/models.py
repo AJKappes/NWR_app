d07d8c912a787817b6ea038be593b698ca4a173d
AJKappes <alexander.j.kappes@gmail.com>
2019-05-09 05:46:52 -0700

    multiple input config

--- nwr_app/models.py
+++ nwr_app/models.py
@@ -10,7 +10,7 @@ from django.forms import ModelForm
 #        fields = '__all__'
 
 # defining the page input for pdf calculation
-class pdf_input(models.Model)
+class pdf_input(models.Model):
     x = models.FloatField(verbose_name=' random variable, x > 0')
     theta = models.FloatField(verbose_name=' scale parameter, theta > 0')
     k = models.FloatField(verbose_name=' shape paramter, k > 0')
