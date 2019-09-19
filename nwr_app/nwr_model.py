8f7d17e886611ad0df9584b1467e6f473987da12
alex <alexander.j.kappes@gmail.com>
2019-09-19 15:48:34 -0700

    update with energy production from input biogas potential

--- nwr_app/nwr_model.py
+++ nwr_app/nwr_model.py
@@ -25,24 +25,32 @@ def phos_func(*args, loc, soil_test, acres):
     Function takes feedstock and manure volume arguments and computes phosphorus nutrient outflow
     in kg/time step.
 
-    3.79 constant converts gallon to liter
-    0.9112 nutrient portion remaining post liquid-solid seperation after AD output to lagoon
+    3.79 constant converts gallon to liter (1L = 1kg)
+    0.9112 nutrient portion remaining post liquid-solid separation after AD output to lagoon
     2.205 constant converts kilogram to pound
+    0.00378541 constant converts mg/ml to kg/gal
+    1000 constant converts kg to cubic meter
+    kWh 1.02264 correction factor, 39.2 calorific value, 3.6 conversion factor
     '''
     coefs = [.035, .107, .34, .011, .13, .023]
     # [blood, fish_by, paper_pulp, ob, gt, manure]
+    bio_coefs = [0.73, 0.475, 0.59, 1.45, 1.9, 0.75]
     tip_fees = [0.03609, 0.05, 0.0415, 0.05, 0.05, 0]
-    # [blood, pulp] tipping fees
     out_list = []
     rev_list = []
+    biogas_list = []
 
     for i in range(len(coefs)):
-        out_list.append(3.79 * coefs[i] * args[i])
+        out_list.append(3.79 * 2.205 * coefs[i] * args[i]) # convert gals to liters to kg to lbs
         rev_list.append(tip_fees[i] * args[i])
+        # convert mg/ml biogas potential to kg/gal for biogas kg output in cubic meters
+        biogas_list.append(0.00378541 * 1000 * bio_coefs[i] * args[i])
 
-    phos_out = round(2.205 * 0.9112 * sum(out_list), 3)
+    phos_out = round(0.9112 * sum(out_list), 3) # proportion remaining post liquid-solid sep
     lb_acre = round(phos_out / acres, 3)
     input_rev = round(sum(rev_list), 3)
+    biogas_pot = round(sum(biogas_list), 3)
+    kWh = round(1.02264 * 39.2 * biogas_pot / 3.6)
 
     if loc == 'West' or loc == 'west':
         if soil_test < 20:
@@ -104,7 +112,7 @@ def phos_func(*args, loc, soil_test, acres):
             rec_p_app = '0'
             app_decision = 'Do not apply'
 
-    return [phos_out, lb_acre, p_index, rec_p_app, app_decision, input_rev]
+    return [phos_out, lb_acre, p_index, rec_p_app, app_decision, input_rev, kWh]
 
 
 #phos_func(10, 15, 20, 25, 30, 35, loc='west', soil_test=51, acres=4)
